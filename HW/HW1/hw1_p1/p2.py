b8358ab263e4db127a8b420dbe76125a558c2655
chun19 <chun3@seas.upenn.edu>
2021-02-12 01:03:11 -0500

    print format update

--- HW/HW1/hw1_p1/p2.py
+++ HW/HW1/hw1_p1/p2.py
@@ -23,6 +23,8 @@ for i in range(1, 20):
 	###
 	alpha = np.hstack((alpha, alpha_1))
 
+print("alpha: \n" + str(alpha.T) + "\n")
+
 total_observation = np.sum(alpha[:, -1], axis = 0);
 
 betas = beta;
@@ -36,17 +38,16 @@ for i in range(0, 20):
 	# print("new beta\n" + str(beta) + "\n")
 	betas = np.hstack((beta, betas))
 betas = betas[:,0:-1]
-print(betas)
+print("beta: \n" + str(betas.T) + "\n")
 
 
 gamma_ = alpha * betas
 gamma_sum = np.sum(gamma_, axis = 0)
 gamma_f = gamma_ / gamma_sum;
-print("gamma: \n" + str(gamma_f))
-print(np.argmax(gamma_f, axis= 0))
+print("gamma: \n" + str(gamma_f.T) + "\n")
+print("Most possible state: \n" + str(np.argmax(gamma_f, axis= 0)) + "\n")
 gamma_sum_col = np.sum(gamma_f[:, :-1], axis = 1)
-print("gamma_sum_col: \n" + str(gamma_sum_col))
-
+# print("gamma_sum_col: \n" + str(gamma_sum_col))
 
 
 T_new = np.zeros((2,2)); 
@@ -55,15 +56,16 @@ for i in range(0, 20 - 1):
 	xi_k = alpha[:,i].reshape(2,1) @ (betas[:,i + 1].reshape(1,2) * M[:, Y[i + 1]].reshape(1,2)) * T.T
 	xi_sum = np.sum(xi_k); 
 	xi_k /= xi_sum;
-	print("XI: ")
+	print("XI" + str(i + 1) + ":")
 	print(xi_k)
 
 	T_new += xi_k.T;
 
-
+print()
 # gamma 2 x 20
 T_new /= gamma_sum_col
-print(T_new)
+
+print("T': \n" + str(T_new) + str("\n"));
 
 M_new = np.zeros((2,3));
 Indicator = np.zeros((20, 3));
@@ -73,11 +75,9 @@ for i in range(3):
 
 gamma_sum_col_full = np.sum(gamma_f, axis = 1)
 
-
 M_new = gamma_f @ Indicator / gamma_sum_col_full.reshape(2,1)
 
-print(M_new)
-
+print("M': \n" + str(M_new) + "\n")
 
 
 alpha_1 = alpha_0 * M[:,Y[0]].reshape(2,1)
@@ -99,7 +99,7 @@ for i in range(1, 20):
 
 total_observation_new = np.sum(alpha_new[:, -1], axis = 0);
 
-print("total_observation: \n" + str(total_observation))
+print("total_observation: \n" + str(total_observation) + "\n")
 print("total_observation_new: \n" + str(total_observation_new))
 
 
